d0c3f8e382dff7c326f1dc4570ba760df9e2e8b7
Alpha-V <alphaverta@gmail.com>
2017-04-18 15:51:59 +0200

    Changed docker file to make it more optimized for using cache.

--- source/app.py
+++ source/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+# Create a flask instance called app
 app = Flask(__name__)
 
+# Render the "homepage"
 @app.route("/")
 def index():
 	return render_template("index.html")
